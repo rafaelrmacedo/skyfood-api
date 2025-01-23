ccbbf3e345e4ddd4193d2d7610d3c39b1fcd193c
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2025-01-23 10:44:16 -0300

    Add method with JPA keyword

--- src/main/java/com/sky/skyfood/domain/repository/CuisineRepository.java
+++ src/main/java/com/sky/skyfood/domain/repository/CuisineRepository.java
@@ -4,9 +4,11 @@ import com.sky.skyfood.domain.entity.Cuisine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CuisineRepository extends JpaRepository<Cuisine, Long> {
 
-//    List<Cuisine> getByName(String name);
+    List<Cuisine> findByName(String name);
 
 }
