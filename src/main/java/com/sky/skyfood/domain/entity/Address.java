2cdb5357df3ee8a970b5af0d101f09f4c70a1113
Rafael Macedo <rafaelrodriguesmacedo2004@gmail.com>
2025-01-29 21:50:46 -0300

    Add Address as an embeddable class for restaurant

--- src/main/java/com/sky/skyfood/domain/entity/Address.java
+++ src/main/java/com/sky/skyfood/domain/entity/Address.java
@@ -1,4 +1,17 @@
 package com.sky.skyfood.domain.entity;
 
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+
+@Data
+@Embeddable // it means that this class can be embedded in another class as an attribute
 public class Address {
+
+    private String cep;
+    private String street;
+    private String number;
+    private String complement;
+    private String neighborhood;
+    private String city;
 }
